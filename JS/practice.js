18a967303b5c21530bb5223db6dff33afb0550b9
masroor07 <76279044+masroor07@users.noreply.github.com>
2022-10-16 11:48:17 +0530

    addeed images to the css project and Started JS

---
+++ JS/practice.js
@@ -0,0 +1,19 @@
+nameMy = "Masroor"
+console.log(nameMy[0])
+console.log(nameMy.length);
+function even(a){
+    if(a%2==0){
+        return true;
+    }
+    return false;
+}
+function add( a,  b){
+    var c =  a+b;
+    return c;
+}
+
+
+var addition = add(50,60)
+var even = even(61);
+console.log(even)
+console.log(addition)
